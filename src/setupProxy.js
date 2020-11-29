a9d2a011825784e1865bdd2794ed183ad941e2fa
Margarets00 <minj01134@gmail.com>
2020-11-30 03:00:14 +0900

    ddd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,6 +7,8 @@ module.exports = function (app) {
       // proxy할 주소, 즉, 백단의 주소를 적어줍니다.
       target: "https://margarets.pythonanywhere.com",
       changeOrigin: true,
+      pathRewrite:{ '^/api/':'/' }
+
     })
   );
 };
